fee0cdfa164f8242d7dac11c0c73330c559c3323
Advay Patil <advaypatil27@gmail.com>
2022-05-21 13:29:38 -0700

    add test for while loop parsing (passed)

--- crates/turbillum/src/while_loop.rs
+++ crates/turbillum/src/while_loop.rs
@@ -32,3 +32,25 @@ impl WhileLoop {
         }))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::boolean_expr::{Boolean, Comparison, ComparisonOp};
+    use crate::expr::block::Block;
+    use crate::expr::{Expr, Number};
+    use super::WhileLoop;
+
+    #[test]
+    fn parse_while_loop() {
+        assert_eq!(WhileLoop::new("$while$[1 >% 2] []"), Ok(("", WhileLoop {
+            condition: Boolean::Comparison(Comparison {
+                lhs: Expr::Number(Number(1)),
+                rhs: Expr::Number(Number(2)),
+                op: ComparisonOp::Geq
+            }),
+            body: Block {
+                stmts: Vec::new()
+            }
+        })));
+    }
+}
